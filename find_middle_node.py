cf0682dc3a98ee12bacb55006f0e5cfd2935bf17
Meghnath Jaishi <meghnathjaishi@gmail.com>
2025-05-26 18:23:58 -0500

    find the middle node in linked list

---
+++ find_middle_node.py
@@ -0,0 +1,31 @@
+## write method to find the middle node of a linked list 
+# if length is odd 
+# or 1st node of second half if length is even
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.head == None:
+            self.head = new_node
+            self.tail = new_node
+        self.tail.next = new_node        
+        self.tail = new_node
+        return True
+    
+    def find_middle(self):
+        slow = self.head
+        fast = self.head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
