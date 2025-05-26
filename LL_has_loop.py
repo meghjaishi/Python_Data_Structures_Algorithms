1c73f6c656e91f51f65a196ba8e0ad11b342dc8d
Meghnath Jaishi <meghnathjaishi@gmail.com>
2025-05-26 18:43:09 -0500

    check if LL has a loop

---
+++ LL_has_loop.py
@@ -0,0 +1,34 @@
+## has_loop
+## check if a linked list has a loop
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+    
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+        return True
+    
+    def has_loop(self):
+        slow = self.head
+        fast = self.head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+        return False
