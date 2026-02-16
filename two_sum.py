4aef042181eb281b27bf92696d24aa420887d47e
Meghnath Jaishi <meghnathjaishi@gmail.com>
2026-02-16 12:36:14 -0600

    two sum problem using a python list

---
+++ two_sum.py
@@ -0,0 +1,31 @@
+
+def two_sum_hash(nums: List[int], target: int) -> List[int]:
+    num_dict = {}
+    for i, num in enumerate(nums):
+        complement = target - num
+        if complement in num_dict:
+            return [num_dict[complement], i]
+        num_dict[num] = i
+    return []
+
+def two_sum_brute_force(self, nums: List[int], target: int) -> List[int]:
+    for i in range(len(nums)):
+        for j in range(i+1, len(nums)):
+            if nums[i] + nums[j] == target:
+                return [i, j]
+    return []
+
+def two_sum_two_pointer(self, nums: List[int], target: int) -> List[int]:
+    left = 0
+    right = len(nums) - 1
+
+    while left < right:
+        current_sum = nums[left] + nums[right]
+        if current_sum == target:
+            return [left, right]
+        elif current_sum < target:
+            left += 1
+        else:
+            right -= 1
+    return []
+
